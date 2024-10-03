946e131e0e4465ea8c9d94983af25d7d2c4a669e
Smith Grisales <developers.smith@gmail.com>
2024-10-03 11:14:55 +0200

    resolve tailwind config

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,15 @@
-// comment 
+// comment
 import React from "react";
 
 // import { useNavigate } from 'react-router-dom'; hay que importar esto para poder usar el useNavigate
 
 const Home = () => {
   return (
-    
     <div>
-    <h1>Home</h1>
-    <p>buenas tardes</p>
+      <h1 className="text-2xl bg-red-500">Home</h1>
+      <p>buenas tardes</p>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
